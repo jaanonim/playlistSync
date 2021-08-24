3873504da73437e8a1121d8a5d88cd255ce7f24a
jaanonim <mat8mro@gmail.com>
2021-08-24 11:49:36 +0200

    better spotify auth

--- spotify/auth.py
+++ spotify/auth.py
@@ -1,7 +1,10 @@
 import json
 import os
 
-file_name = "token.json"
+import spotipy
+import spotipy.oauth2 as oauth2
+
+file_name = "spotify.json"
 
 
 def auth():
@@ -9,19 +12,20 @@ def auth():
     try:
         f = open(file_name)
         data = json.load(f)
-        token = data["token"]
         f.close()
-        if token == None:
+        if data == {}:
             raise Exception
     except Exception as e:
-        print("https://developer.spotify.com/console/get-several-albums/")
-        token = input("Enter spotify token: ")
-        with open(file_name, "w") as json_file:
-            json.dump({"token": token}, json_file)
-    return token
+        print("Cannot found " + file_name)
+        exit()
+
+    credentials = oauth2.SpotifyClientCredentials(
+        client_id=data["clientID"], client_secret=data["clientSecret"]
+    )
+    token = credentials.get_access_token()
+    return token["access_token"]
 
 
 def onInvalidToken():
     print("ERROR: Invalid token!")
-    os.remove(file_name)
     exit()
